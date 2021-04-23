d93c36e2b217122ef1dfd8b2e21f1a8a7597bc10
周壬癸 <729049182@qq.com>
2021-04-23 22:28:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello VuePress!
+
+这是一个演示项目。
+
+shell deploy 成功
+
+drone deploy 成功
---
+++ scripts/deploy.js
@@ -0,0 +1,77 @@
+const colors = require("colors")
+const shell = require("shelljs")
+const Rsync = require("rsync")
+const path = require("path")
+const ora = require("ora")
+const spinner = ora("Loading unicorns").start()
+const yargs = require("yargs/yargs")
+const { hideBin } = require("yargs/helpers")
+const argv = yargs(hideBin(process.argv)).argv
+
+const hostsMap = {
+  prod: "baiduyun",
+}
+
+function sendMessage(message) {
+  shell.exec(`
+    curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=e6ad01ff-7207-4183-abfa-4c89fa802177' \
+    -H 'Content-Type: application/json' \
+    -d '
+    {
+      "msgtype": "text",
+      "text": {
+          "content": "${message}"
+      }
+    }'
+  `)
+}
+
+if (!argv.th) {
+  console.log()
+  console.log(colors.yellow("测试主机不存在"))
+}
+
+// 通知构建
+sendMessage("开始构建")
+
+// 安装依赖
+console.log()
+console.log(colors.green("安装依赖"))
+if (shell.exec("npm install").code !== 0) {
+  shell.echo("Error: npm install failed")
+  shell.exit(1)
+}
+
+// 测试
+console.log()
+console.log(colors.green("开始测试"))
+if (shell.exec("npm run test").code !== 0) {
+  shell.echo("Error: npm run test failed")
+  shell.exit(1)
+}
+
+// 构建
+console.log()
+console.log(colors.green("开始构建"))
+if (shell.exec("npm run build").code !== 0) {
+  shell.echo("Error: npm run build failed")
+  shell.exit(1)
+}
+
+// 部署
+console.log()
+console.log(colors.green("开始部署"))
+const rsync = Rsync.build({
+  source: path.resolve(__dirname, "../.vuepress/dist/"),
+  destination: `${hostsMap["prod"]}:/home/doc/`,
+  flags: "avz",
+  shell: "ssh",
+})
+
+rsync.execute(function(err, code, cmd) {
+  console.error(err, code, cmd)
+})
+
+// 通知
+sendMessage("部署成功")
+spinner.stop()
